14df834b6a1301b9e052044f5d2c8e385bf9d9eb
Ji Ho Koo <jihokoo@gmail.com>
2015-07-08 16:55:25 -0400

    add Tag model and associate with Factory

---
+++ crud/migrations/0002_auto_20150708_1541.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crud', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.RenameField(
+            model_name='address',
+            old_name='address_1',
+            new_name='line_1',
+        ),
+        migrations.RenameField(
+            model_name='address',
+            old_name='address_2',
+            new_name='line_2',
+        ),
+        migrations.AddField(
+            model_name='factory',
+            name='tags',
+            field=models.ManyToManyField(to='crud.Tag'),
+        ),
+    ]
--- crud/models.py
+++ crud/models.py
@@ -1,19 +1,26 @@
 from django.db import models
 
+class Factory(models.Model):
+  name = models.CharField(max_length=50)
+  email = models.CharField(max_length=50)
+  address = models.ForeignKey("Address")
+  tags = models.ManyToManyField("Tag")
+
+  def __str__(self):
+    return self.name
+
 class Address(models.Model):
-  address_1 = models.CharField(max_length=50)
-  address_2 = models.CharField(max_length=50)
+  line_1 = models.CharField(max_length=50)
+  line_2 = models.CharField(max_length=50)
   city = models.CharField(max_length=50)
   state = models.CharField(max_length=2)
   zipcode = models.CharField(max_length=5)
 
   def __str__(self):
-    return self.address_1
+    return self.line_1
 
-class Factory(models.Model):
-  address = models.ForeignKey(Address)
+class Tag(models.Model):
   name = models.CharField(max_length=50)
-  email = models.CharField(max_length=50)
 
   def __str__(self):
     return self.name
