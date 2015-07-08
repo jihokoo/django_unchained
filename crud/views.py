ebc4bcb6144e8b708c4b5bbd1d9282ac9e7ae25a
Ji Ho Koo <jihokoo@gmail.com>
2015-07-08 19:22:19 -0400

    add index view

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortucts import render
 
-# Create your views here.
+from .models import Factory
+
+def index (request):
+  latest_factory_list = Factory.objects.order_by('name')
+  context = {'latest_factory_list': latest_factory_list}
+  return render(request, 'crud/index.html', context)
