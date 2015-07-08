07f983d366a8f959f8168c21ae8589f6e5ea56dc
Ji Ho Koo <jihokoo@gmail.com>
2015-07-07 23:40:06 -0400

    Create Address and Factory models

---
+++ crud/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Address',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('address_1', models.CharField(max_length=50)),
+                ('address_2', models.CharField(max_length=50)),
+                ('city', models.CharField(max_length=50)),
+                ('state', models.CharField(max_length=2)),
+                ('zipcode', models.CharField(max_length=5)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Factory',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('email', models.CharField(max_length=50)),
+                ('address', models.ForeignKey(to='crud.Address')),
+            ],
+        ),
+    ]
--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+  address_1 = models.CharField(max_length=50)
+  address_2 = models.CharField(max_length=50)
+  city = models.CharField(max_length=50)
+  state = models.CharField(max_length=2)
+  zipcode = models.CharField(max_length=5)
+
+  def __str__(self):
+    return self.address_1
+
+class Factory(models.Model):
+  address = models.ForeignKey(Address)
+  name = models.CharField(max_length=50)
+  email = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
