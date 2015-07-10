a76cb0d20d5207048e1c2216d44fe8b2713453e6
Ji Ho Koo <jihokoo@gmail.com>
2015-07-10 12:19:00 -0400

    add delete, create, update factories

--- factory/urls.py
+++ factory/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<factory_id>[0-9]+)/$', views.show, name='show'),
-    url(r'^new$', views.new, name='new'),
+    url(r'^$', views.showAll, name='showAll'),
     url(r'^create$', views.create, name='create'),
-    url(r'^(?P<factory_id>[0-9]+)/edit$', views.edit, name='edit'),
-    url(r'^update$', views.update, name='update'),
+    url(r'^(?P<factory_id>[0-9]+)/$', views.showOne, name='showOne'),
+    url(r'^(?P<factory_id>[0-9]+)/update$', views.update, name='update'),
+    url(r'^(?P<factory_id>[0-9]+)/delete$', views.delete, name='delete'),
 ]
--- factory/views.py
+++ factory/views.py
@@ -1,29 +1,66 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 
+from django.core.urlresolvers import reverse
 
-from .models import Factory
+from .models import Factory, Address
 
-def index (request):
-  latest_factory_list = Factory.objects.order_by('name')
-  context = {'latest_factory_list': latest_factory_list}
-  return render(request, 'factory/index.html', context)
+def showAll (request):
+  if request.method == 'GET':
+    latest_factory_list = Factory.objects.order_by('name')
+    context = {'latest_factory_list': latest_factory_list}
+    return render(request, 'factory/showAll.html', context)
 
-def show (request, factory_id):
-  factory = get_object_or_404(Factory, id=factory_id)
-  return render(request, 'factory/show.html', {'factory': factory})
+def showOne (request, factory_id):
+  if request.method == 'GET':
+    factory = get_object_or_404(Factory, id=factory_id)
+    return render(request, 'factory/showOne.html', {'factory': factory})
 
-def new (request):
-  return render(request, 'factory/new.html')
 
 def create (request):
-  return HttpResponseRedirect(reverse('factory:index'))
+  if request.method == 'GET':
+    return render(request, 'factory/create.html')
+  elif request.method == 'POST':
+    address = Address(
+      line_1 = request.POST["line_1"],
+      line_2 = request.POST["line_2"],
+      city = request.POST["city"],
+      state = request.POST["state"],
+      zipcode = request.POST["zipcode"]
+    )
+    address.save()
 
-def edit (request, factory_id):
-  factory = get_object_or_404(Factory, id=factory_id)
-  return render(request, 'factory/edit.html', {'factory': factory})
+    factory = Factory(
+      name = request.POST["name"],
+      email = request.POST["email"],
+      address = address,
+    )
+    factory.save()
+
+    return HttpResponseRedirect(reverse('factory:showOne', args=(factory.id,)))
 
 def update (request, factory_id):
   factory = get_object_or_404(Factory, id=factory_id)
-  return HttpResponseRedirect(reverse('factory:index', args=(factory,)))
+
+  if request.method == 'GET':
+    return render(request, 'factory/update.html', {'factory': factory})
+  elif request.method == 'POST':
+
+    address = factory.address
+    address.line_1 = request.POST["line_1"]
+    address.line_2 = request.POST["line_2"]
+    address.city = request.POST["city"]
+    address.state = request.POST["state"]
+    address.zipcode = request.POST["zipcode"]
+    address.save()
+
+    factory.name = request.POST["name"]
+    factory.email = request.POST["email"]
+    factory.save()
+
+    return HttpResponseRedirect(reverse('factory:showOne', args=(factory.id,)))
+
+def delete (request, factory_id):
+  get_object_or_404(Factory, id=factory_id).delete()
+  return HttpResponseRedirect(reverse('factory:showAll'))
 
