394fcee23497b199b1d4abf9ab8bbed232425f7c
Ji Ho Koo <jihokoo@gmail.com>
2015-07-11 15:48:20 -0400

    refactor crud app to consume factory service

--- factory/views.py
+++ factory/views.py
@@ -1,96 +1,48 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
-from django.db import IntegrityError
 
-from .models import Factory, Address, Tag
+from utilities.services import factory_service
 
 def showAll (request):
   if request.method == 'GET':
-    latest_factory_list = Factory.objects.order_by('name')
+    latest_factory_list = factory_service.getAll()
     context = {'latest_factory_list': latest_factory_list}
     return render(request, 'factory/showAll.html', context)
 
 def showOne (request, factory_id):
-  if request.method == 'GET':
-    factory = get_object_or_404(Factory, id=factory_id)
-    return render(request, 'factory/showOne.html', {'factory': factory})
-
+  try:
+    if request.method == 'GET':
+      factory = factory_service.getOne(factory_id)
+      return render(request, 'factory/showOne.html', {'factory': factory})
+  except ObjectDoesNotExist:
+    raise Http404('Factory with id ' + factory_id + ' does not exist')
 
 def create (request):
   if request.method == 'GET':
     return render(request, 'factory/create.html')
   elif request.method == 'POST':
-    address = Address(
-      line_1 = request.POST["line_1"],
-      line_2 = request.POST["line_2"],
-      city = request.POST["city"],
-      state = request.POST["state"],
-      zipcode = request.POST["zipcode"]
-    )
-    address.save()
-
-    factory = Factory(
-      name = request.POST["name"],
-      email = request.POST["email"],
-      address = address,
-    )
-    factory.save()
-
-    tags = request.POST.get('tags', 0)
-    if tags != 0:
-      tags = tags.split(',')
-      for name in tags:
-        name = name.lower()
-        try:
-          tag = Tag(name=name)
-          tag.save()
-        except IntegrityError as e:
-          if 'unique constraint' in e.message.lower():
-            tag = Tag.objects.get(name=name)
-        factory.tags.add(tag)
+    factory = factory_service.create(request.POST)
 
     return HttpResponseRedirect(reverse('factory:showOne', args=(factory.id,)))
 
 def update (request, factory_id):
-  factory = get_object_or_404(Factory, id=factory_id)
-
-  if request.method == 'GET':
-    return render(request, 'factory/update.html', {'factory': factory})
-  elif request.method == 'POST':
-
-    address = factory.address
-    address.line_1 = request.POST["line_1"]
-    address.line_2 = request.POST.get('line_2', '')
-    address.city = request.POST["city"]
-    address.state = request.POST["state"]
-    address.zipcode = request.POST["zipcode"]
-    address.save()
-
-    tags = request.POST.get('tags', 0)
-    if tags != 0:
-      tags = tags.split(',')
-      for name in tags:
-        name = name.lower()
-        try:
-          tag = Tag(name = name)
-          tag.save()
-          factory.tags.add(tag)
-        except IntegrityError as e:
-          if 'unique constraint' in e.message.lower():
-            tag = Tag.objects.get(name = name)
-            for existing in factory.tags:
-              if (tags.index(existing.name) == -1):
-                factory.tags.add(tag)
-
-    factory.name = request.POST["name"]
-    factory.email = request.POST["email"]
-    factory.save()
-
-    return HttpResponseRedirect(reverse('factory:showOne', args=(factory.id,)))
+  try:
+    if request.method == 'GET':
+      factory = factory_service.getOne(factory_id)
+      return render(request, 'factory/update.html', {'factory': factory})
+    elif request.method == 'POST':
+      factory = factory_service.update(factory_id, request.POST)
+      return HttpResponseRedirect(reverse('factory:showOne', args=(factory.id,)))
+  except ObjectDoesNotExist:
+    raise Http404('Factory with id ' + factory_id + ' does not exist')
 
 def delete (request, factory_id):
-  get_object_or_404(Factory, id=factory_id).delete()
-  return HttpResponseRedirect(reverse('factory:showAll'))
+  try:
+    factory_service.delete(factory_id)
+    return HttpResponseRedirect(reverse('factory:showAll'))
+  except ObjectDoesNotExist:
+    raise Http404('Factory with id ' + factory_id + ' does not exist')
 
---
+++ utilities/services/factory_service.py
@@ -0,0 +1,97 @@
+from django.core.urlresolvers import reverse
+from django.db import IntegrityError
+
+from factory.models import Factory, Address, Tag
+
+def getAll ():
+  latest_factory_list = Factory.objects.order_by('name')
+  return latest_factory_list
+
+# factory_id: string
+def getOne (factory_id):
+  try:
+    factory = Factory.objects.get(id = factory_id)
+    return factory
+  except Factory.DoesNotExist:
+    raise
+
+# factory_id: string, factory_data: QuerySet
+def create (factory_data):
+  address = Address(
+    line_1 = factory_data['line_1'],
+    line_2 = factory_data.get('line_2', None),
+    city = factory_data['city'],
+    state = factory_data['state'],
+    zipcode = factory_data['zipcode']
+  )
+  address.save()
+
+  factory = Factory(
+    name = factory_data['name'],
+    email = factory_data['email'],
+    address = address,
+  )
+  factory.save()
+
+  tags = factory_data.get('tags', 0)
+  if tags != 0:
+    tags = tags.split(',')
+    for name in tags:
+      name = name.lower()
+      try:
+        tag = Tag(name=name)
+        tag.save()
+      except IntegrityError as e:
+        if 'unique constraint' in e.message.lower():
+          tag = Tag.objects.get(name=name)
+      factory.tags.add(tag)
+
+  return factory
+
+# factory_id: string, factory_data: QuerySet
+def update (factory_id, factory_data):
+  try:
+    factory = Factory.objects.get(id = factory_id)
+  except Factory.DoesNotExist:
+    raise
+
+  address = factory.address
+  address.line_1 = factory_data['line_1']
+  address.line_2 = factory_data.get('line_2', None)
+  address.city = factory_data['city']
+  address.state = factory_data['state']
+  address.zipcode = factory_data['zipcode']
+  address.save()
+
+  tags = factory_data.get('tags', 0)
+  if tags != 0:
+    tags = tags.split(',')
+    for name in tags:
+      name = name.lower()
+      try:
+        tag = Tag(name = name)
+        tag.save()
+        factory.tags.add(tag)
+      except IntegrityError as e:
+        if 'unique constraint' in e.message.lower():
+          tag = Tag.objects.get(name = name)
+          for existing in factory.tags.all():
+            try:
+              if (tags.index(existing.name.encode('ascii', 'ignore'))):
+                factory.tags.add(tag)
+            except ValueError:
+              continue
+
+  factory.name = factory_data['name']
+  factory.email = factory_data['email']
+  factory.save()
+
+  return factory
+
+# factory_id: string
+def delete (factory_id):
+  try:
+    factory = Factory.objects.get(id = factory_id)
+    factory.delete()
+  except Factory.DoesNotExist:
+    raise
