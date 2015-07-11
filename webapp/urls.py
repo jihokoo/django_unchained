d0e9b8a967b3a628eafd5858e0c9dc1a30fedf62
Ji Ho Koo <jihokoo@gmail.com>
2015-07-11 18:01:54 -0400

    move webapp/views.py into controller sub-directory

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 
-from . import views
+from .controllers import factory
 
 urlpatterns = [
   # web app
-  url(r'^factory/$', views.showAll, name='factory.showAll'),
-  url(r'^factory/create$', views.create, name='factory.create'),
-  url(r'^factory/(?P<factory_id>[0-9]+)/$', views.showOne, name='factory.showOne'),
-  url(r'^factory/(?P<factory_id>[0-9]+)/update$', views.update, name='factory.update'),
-  url(r'^factory/(?P<factory_id>[0-9]+)/delete$', views.delete, name='factory.delete'),
+  url(r'^factory/$', factory.showAll, name='factory.showAll'),
+  url(r'^factory/create$', factory.create, name='factory.create'),
+  url(r'^factory/(?P<factory_id>[0-9]+)/$', factory.showOne, name='factory.showOne'),
+  url(r'^factory/(?P<factory_id>[0-9]+)/update$', factory.update, name='factory.update'),
+  url(r'^factory/(?P<factory_id>[0-9]+)/delete$', factory.delete, name='factory.delete'),
 ]
